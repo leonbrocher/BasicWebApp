7dcba44e534ec97ab32df37961b4968ea1e28e01
leon <leonb1206@gmail.com>
2022-07-14 13:25:29 +0200

    largest

--- src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
+++ src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
@@ -9,40 +9,6 @@ class QueryProcessorTest {
 
 	private final QueryProcessor queryProcessor = new QueryProcessor();
 
-	@Test
-	void testEmptyStringIfCannotProcessQuery() {
-		assertEquals("", queryProcessor.process("test"));
-	}
 
-	@Test
-	void testKnowsAboutShakespeare() {
-		String actual = queryProcessor.process("Shakespeare");
-		if (!actual.contains("playwright")) {
-			fail("The QueryProcessor does not know about Shakespeare.");
-		}
-	}
-	@Test
-	void plus() {
-		String actual = queryProcessor.process("what is 20 plus 15");
-		System.out.println(actual);
-		if (!actual.equals("35")) {
-			fail("The QueryProcessor does not know about Shakespeare.");
-		}
-	}
-	@Test
-	void largest() {
-		String actual = queryProcessor.process("%20which%20of%20the%20following%20numbers%20is%20the%20largest:%20601,%2025");
-		if (!actual.equals("601")) {
-			fail("The QueryProcessor does not know about Shakespeare.");
-		}
-	}
-
-	@Test
-	void isNotCaseSensitive() {
-		String actual = queryProcessor.process("shakespeare");
-		if (!actual.contains("playwright")) {
-			fail("Your QueryProcessor should not be case sensitive.");
-		}
-	}
 
 }
